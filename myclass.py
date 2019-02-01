ea27e1e8d5d3d8457bc0c93e611f1785d6c14ff2
Esenova <47048618+Esenova@users.noreply.github.com>
2019-02-01 15:29:49 +0600

    Child class

--- myclass.py
+++ myclass.py
@@ -1,7 +1,23 @@
-class SumTwoNumbers(object):
-    def __init__(self, num_1, num_2):
-        self.num_1 = num_1
-        self.num_2 = num_2
+class Plov():
+    def __init__(self, plov_type, name='Плов', rice=True, meat=True):
+        self.plov_type = plov_type
+        self.name = name
+        self.rice = rice
+        self.meat = meat
 
-    def get_sum(self):
-        return self.num_1 + self.num_2
+    def get_portion(self):
+    	print(f'1 portion of {self.plov_type} {self.name} is ready!')
+
+    def add_carrot(self):
+    	print(f'carrot added to the {name}')
+
+
+class Uzbek_Plov(Plov):
+	def __init__(self, plov_type, name='Плов', rice=True, meat=True):
+		self.plov_type = plov_type
+		self.name = name
+		self.rice = rice
+		self.meat = meat
+
+	def add_carrot(self):
+		print(f'No carrot in {self.plov_type} {self.name}')
